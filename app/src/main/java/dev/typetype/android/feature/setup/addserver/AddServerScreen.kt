46111926bae9b74bc2b52184003b1482530c26de
Priveetee <brahim.benyoucefpro@proton.me>
2026-05-03 15:40:56 +0200

    refactor: migrate hiltViewModel to lifecycle viewmodel package

--- app/src/main/java/dev/typetype/android/feature/setup/addserver/AddServerScreen.kt
+++ app/src/main/java/dev/typetype/android/feature/setup/addserver/AddServerScreen.kt
@@ -43,7 +43,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.typetype.android.R
 import dev.typetype.android.core.ui.components.SectionHeader
--- app/src/main/java/dev/typetype/android/feature/setup/login/LoginScreen.kt
+++ app/src/main/java/dev/typetype/android/feature/setup/login/LoginScreen.kt
@@ -29,7 +29,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.typetype.android.core.ui.components.SectionHeader
 import dev.typetype.android.core.ui.components.TypeTypeCard
