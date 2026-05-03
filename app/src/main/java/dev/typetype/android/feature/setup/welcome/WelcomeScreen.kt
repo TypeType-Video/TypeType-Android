fbd33e7b22e239450987a6f8ea1dd8a8b7156d5f
Priveetee <brahim.benyoucefpro@proton.me>
2026-05-03 15:40:56 +0200

    refactor: migrate hiltViewModel to lifecycle viewmodel package

--- app/src/main/java/dev/typetype/android/feature/setup/welcome/WelcomeScreen.kt
+++ app/src/main/java/dev/typetype/android/feature/setup/welcome/WelcomeScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import dev.typetype.android.R
 import dev.typetype.android.core.ui.components.SectionHeader
 import dev.typetype.android.core.ui.components.TypeTypePrimaryButton
