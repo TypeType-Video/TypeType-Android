e1adda1b74ad98bde20b661ec35110de14b544f7
Priveetee <brahim.benyoucefpro@proton.me>
2026-05-03 09:56:37 +0200

    feat(setup): Login MVI (Action, State, Event, ViewModel)

---
+++ app/src/main/java/dev/typetype/android/feature/setup/login/LoginAction.kt
@@ -0,0 +1,9 @@
+package dev.typetype.android.feature.setup.login
+
+sealed interface LoginAction {
+    data class OnIdentifierChange(val value: String) : LoginAction
+    data class OnPasswordChange(val value: String) : LoginAction
+    data object OnLoginClick : LoginAction
+    data object OnContinueAsGuestClick : LoginAction
+    data object OnBackClick : LoginAction
+}
