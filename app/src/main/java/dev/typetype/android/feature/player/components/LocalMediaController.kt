21cba1ddcfef4297e84f16aa634ec58296fd73b7
Priveetee <brahim.benyoucefpro@proton.me>
2026-05-03 16:40:21 +0200

    feat(player): add mini player bar and media controller composition local

---
+++ app/src/main/java/dev/typetype/android/feature/player/components/LocalMediaController.kt
@@ -0,0 +1,6 @@
+package dev.typetype.android.feature.player.components
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.media3.session.MediaController
+
+val LocalMediaController = compositionLocalOf<MediaController?> { null }
