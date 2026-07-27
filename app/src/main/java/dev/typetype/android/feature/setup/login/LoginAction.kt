dae020687b3ffbcab635a9a519ea9f5ec97ed022
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-27 08:15:38 +0200

    feat: support password and OIDC login

--- app/src/main/java/dev/typetype/android/feature/setup/login/LoginAction.kt
+++ app/src/main/java/dev/typetype/android/feature/setup/login/LoginAction.kt
@@ -4,6 +4,10 @@ sealed interface LoginAction {
     data class OnIdentifierChange(val value: String) : LoginAction
     data class OnPasswordChange(val value: String) : LoginAction
     data object OnLoginClick : LoginAction
+    data object OnOidcClick : LoginAction
+    data class OnOidcCallback(val callbackUrl: String) : LoginAction
+    data object OnOidcBrowserUnavailable : LoginAction
+    data object OnOidcCancelled : LoginAction
     data object OnContinueAsGuestClick : LoginAction
     data object OnBackClick : LoginAction
 }
