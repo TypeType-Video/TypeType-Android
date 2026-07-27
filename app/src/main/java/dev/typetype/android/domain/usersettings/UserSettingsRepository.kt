9d5e41ddee7ef21f8662c0772e607ad09dd05315
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-27 08:08:47 +0200

    feat: expose scoped user preferences

--- app/src/main/java/dev/typetype/android/domain/usersettings/UserSettingsRepository.kt
+++ app/src/main/java/dev/typetype/android/domain/usersettings/UserSettingsRepository.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface UserSettingsRepository {
     fun observe(): Flow<UserSettings>
+    suspend fun current(): Result<UserSettings>
     suspend fun refresh(): Result<Unit>
     suspend fun update(transform: (UserSettings) -> UserSettings): Result<Unit>
 }
