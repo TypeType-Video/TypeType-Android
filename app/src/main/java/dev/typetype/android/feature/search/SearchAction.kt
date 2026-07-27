decb2d7d9f1b96ca5bce178e7722ac444d5950d9
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-27 08:15:38 +0200

    feat: coordinate filtered searches

--- app/src/main/java/dev/typetype/android/feature/search/SearchAction.kt
+++ app/src/main/java/dev/typetype/android/feature/search/SearchAction.kt
@@ -5,6 +5,9 @@ sealed interface SearchAction {
     data object OnSearch : SearchAction
     data class OnSuggestionClick(val query: String) : SearchAction
     data object OnClearQuery : SearchAction
+    data class OnContentFilterSelect(val value: String?) : SearchAction
+    data class OnSortFilterSelect(val value: String?) : SearchAction
+    data object OnLoadMore : SearchAction
     data class OnDeleteHistoryEntry(val query: String) : SearchAction
     data class OnHistoryEntryClick(val query: String) : SearchAction
 }
