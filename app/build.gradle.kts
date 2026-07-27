42c91fff0f2f01d0083d8d7d16a9e14cd33dd54a
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-27 22:01:19 +0200

    build: release version 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10106
-        versionName = "1.1.6"
+        versionCode = 10107
+        versionName = "1.1.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
