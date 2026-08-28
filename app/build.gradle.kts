1d0a454c13531e1ac8f0622c105bc8c442a461dd
Priveetee <brahim.benyoucefpro@proton.me>
2026-08-28 15:13:34 +0200

    fix: make beta APK compatible with F-Droid scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10700
-        versionName = "1.7.0-beta.1"
+versionCode = 10701
+versionName = "1.7.0-beta.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
