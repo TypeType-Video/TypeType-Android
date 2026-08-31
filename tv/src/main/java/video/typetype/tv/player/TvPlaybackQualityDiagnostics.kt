e97e1d113cc99047459b3232e9646f984f8f50d4
Priveetee <brahim.benyoucefpro@proton.me>
2026-08-31 23:41:25 +0200

    feat: measure TV playback quality

---
+++ tv/src/main/java/video/typetype/tv/player/TvPlaybackQualityDiagnostics.kt
@@ -0,0 +1,13 @@
+package video.typetype.tv.player
+
+import android.util.Log
+
+internal fun logTvPlaybackQuality(metrics: TvPlaybackQualityMetrics) {
+    val snapshot = metrics.snapshot()
+    Log.i(
+        "TypeTypePlayback",
+        "quality startupMs=${snapshot.startupMilliseconds ?: -1L} " +
+            "playedMs=${snapshot.playedMilliseconds} rebufferMs=${snapshot.rebufferMilliseconds} " +
+            "droppedVideoFrames=${snapshot.droppedVideoFrames}",
+    )
+}
