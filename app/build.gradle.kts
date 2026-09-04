a829905a612f04e276d7b62d309ba1844e7e399d
Priveetee <brahim.benyoucefpro@proton.me>
2026-09-04 17:13:43 +0200

    chore: prepare beta 1.7.0-beta.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10712
-        versionName = "1.7.0-beta.13"
+        versionCode = 10713
+        versionName = "1.7.0-beta.14"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
