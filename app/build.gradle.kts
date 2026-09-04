f6b75af197643402652876d439f49479d9e95f23
Priveetee <brahim.benyoucefpro@proton.me>
2026-09-04 12:19:47 +0200

    chore: bump beta to 1.7.0-beta.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10601
-        versionName = "1.6.1"
+        versionCode = 10711
+        versionName = "1.7.0-beta.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
