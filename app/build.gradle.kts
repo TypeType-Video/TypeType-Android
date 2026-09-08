8a4dde30dae842c18723403e6c166d079be3f352
Priveetee <brahim.benyoucefpro@proton.me>
2026-09-08 18:54:05 +0200

    build: advance mobile development to 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10717
-        versionName = "1.7.0-dev"
+        versionCode = 10801
+        versionName = "1.8.0-dev"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
