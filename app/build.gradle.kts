c4c6912ab56db2a645bf8f25a1457222c9bc3a2e
Priveetee <brahim.benyoucefpro@proton.me>
2026-09-16 11:25:16 +0200

    build: publish mobile beta 1.8.0-beta.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10812
-        versionName = "1.8.0-beta.12"
+        versionCode = 10813
+        versionName = "1.8.0-beta.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
