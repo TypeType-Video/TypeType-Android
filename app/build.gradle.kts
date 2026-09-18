8fece92f093dec96e6fc781dbd22d25734c3d2f9
Priveetee <brahim.benyoucefpro@proton.me>
2026-09-18 10:18:08 +0200

    build: prepare Android 1.8.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.typetype.android"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10819
-        versionName = "1.8.0-beta.19"
+        versionCode = 10820
+        versionName = "1.8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "TypeType")
     }
